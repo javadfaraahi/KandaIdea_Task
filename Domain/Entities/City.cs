07ccb16d03ecd623d064ef6150cd1caca6b22527
Javad Faraahi <javadfaraahi@gmail.com>
2025-04-15 01:00:21 +0330

    Crerate User, Province and City entities

---
+++ Domain/Entities/City.cs
@@ -0,0 +1,10 @@
+﻿namespace KandaIdea_Task.Domain.Entities
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProvinceId   { get; set; }
+        public Province Province { get; set; }
+    }
+}
---
+++ Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿namespace KandaIdea_Task.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public Province Province{ get; set; }
+        public int ProvinceId { get; set; }
+        public City City { get; set; }
+        public int CityId { get; set; }
+    }
+}
