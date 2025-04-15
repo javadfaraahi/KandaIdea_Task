0aefc1c3ea7b49a607bb9f38e7a08c168f936d9c
Javad Faraahi <javadfaraahi@gmail.com>
2025-04-16 00:26:12 +0330

    add models

--- Domain/Entities/City.cs
+++ Domain/Entities/City.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int ProvinceId   { get; set; }
-        public Province Province { get; set; }
+        public virtual Province Province { get; set; }
     }
 }
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -6,9 +6,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
-        public Province Province{ get; set; }
-        public int ProvinceId { get; set; }
-        public City City { get; set; }
+        public virtual City City { get; set; }
         public int CityId { get; set; }
     }
 }
