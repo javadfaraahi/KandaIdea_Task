1e98cae346bd017cdeebae8f74048277755e3104
Javad Faraahi <javadfaraahi@gmail.com>
2025-04-17 19:30:48 +0330

    Create Province Dto

---
+++ Application/DTOs/ProvinceDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace KandaIdea_Task.Application.DTOs
+{
+    public class ProvinceDTO
+    {
+        public string ProvinceName { get; set; }
+        public List<string> Cities { get; set; }
+    }
+}
