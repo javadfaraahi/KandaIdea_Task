422c5714740eb03e6e7fac9e84c0c566a345cb64
javadfaraahi@gmail.com <javadfaraahi@gmail.com>
2025-04-17 09:23:18 +0330

    Configure DbContext

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using KandaIdea_Task.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KandaIdea_Task.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options ) : base ( options )
+        {
+            
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Province> Provinces { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using KandaIdea_Task.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("kanda")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
