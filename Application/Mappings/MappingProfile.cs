ff827446045ebb69b58a519088809f2114446064
javadfaraahi@gmail.com <javadfaraahi@gmail.com>
2025-04-17 10:48:26 +0330

    Add and configure AutoMapper

---
+++ Application/Mappings/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using KandaIdea_Task.Application.DTOs;
+using KandaIdea_Task.Domain.Entities;
+namespace KandaIdea_Task.Application.Mappings;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<User, UserDto>();
+        CreateMap<UserCreateDto, User>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
+using DNTPersianUtils.Core.IranCities;
+using KandaIdea_Task.Application.Mappings;
+using KandaIdea_Task.Extensions;
 using KandaIdea_Task.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("kanda")));
+//AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+// Run DataSeeder
+await app.UseDatabaseSeederAsync();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
