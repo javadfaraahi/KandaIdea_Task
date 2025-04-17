19510928c493fd857d53b757cc006ea712f0a372
Javad Faraahi <javadfaraahi@gmail.com>
2025-04-17 19:07:56 +0330

    Add QueryWithIncludes to IRepository and Repository

--- Infrastructure/Repositories/Repository.cs
+++ Infrastructure/Repositories/Repository.cs
@@ -1,32 +1,36 @@
 ﻿using KandaIdea_Task.Domain.Interfaces;
 using KandaIdea_Task.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace KandaIdea_Task.Infrastructure.Repositories
 {
     public class Repository<T> : IRepository<T> where T : class
     {
         #region Fields
-        private readonly AppDbContext _context;
+        protected readonly AppDbContext _context;
         private readonly DbSet<T> _dbSet;
         #endregion
         #region Ctor
         public Repository(AppDbContext context)
         {
             _context = context;
-            _dbSet = _context.Set<T>(); 
+            _dbSet = _context.Set<T>();
         }
         #endregion
-        public virtual async Task AddAsync(T entity)
+        public virtual async Task<T> AddAsync(T entity)
         {
             await _dbSet.AddAsync(entity);
             await _context.SaveChangesAsync();
+            return entity;
         }
 
         public virtual IQueryable<T> AsQueryable()
         {
             return _dbSet.AsQueryable();
         }
+      
+
 
         public virtual async Task DeleteAsync(int id)
         {
@@ -48,7 +52,7 @@ namespace KandaIdea_Task.Infrastructure.Repositories
                     entities.Add(entity);
                 }
             }
-             _dbSet.RemoveRange(entities);
+            _dbSet.RemoveRange(entities);
             await _context.SaveChangesAsync();
         }
 
@@ -64,17 +68,33 @@ namespace KandaIdea_Task.Infrastructure.Repositories
 
         public virtual async Task<List<T>> GetByConditionAsync(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
         {
-            return await _dbSet.Where(predicate).ToListAsync(); 
+            return await _dbSet.Where(predicate).ToListAsync();
         }
 
         public virtual async Task UpdateAsync(T entity)
         {
-             _dbSet.Update(entity);
+            _dbSet.Update(entity);
             await _context.SaveChangesAsync();
         }
         public virtual async Task SaveChangesAsync()
         {
             await _context.SaveChangesAsync();
+        }
+
+       
+        public IQueryable<T> QueryWithIncludes(Expression<Func<T, bool>> predicate,
+            params Expression<Func<T, object>>[] includes)
+        {
+            IQueryable<T> query = _dbSet;
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+            foreach (var include in includes)
+            {
+                query = query.Include(include);
+            }
+            return query;
         }
     }
 }
