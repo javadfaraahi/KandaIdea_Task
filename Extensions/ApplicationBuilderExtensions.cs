fc2f450b96e0d5f61cfc8b0df0e8b2853b77ca67
javadfaraahi@gmail.com <javadfaraahi@gmail.com>
2025-04-17 10:48:13 +0330

    Add application builder extensions

---
+++ Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using KandaIdea_Task.Infrastructure.Data;
+
+namespace KandaIdea_Task.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static async Task UseDatabaseSeederAsync (this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope ();
+            var context = scope.ServiceProvider.GetRequiredService<AppDbContext> ();
+            await DataSeeder.EnsureSeededAsync (context);
+        }
+    }
+}
