6ace4029e6a20bd4c16e9700441f6c3c3f71cd0d
Javad Faraahi <javadfaraahi@gmail.com>
2025-04-17 19:11:04 +0330

    Complete UserController (route + all methods)

--- Program.cs
+++ Program.cs
@@ -1,23 +1,29 @@
 using DNTPersianUtils.Core.IranCities;
 using KandaIdea_Task.Application.Mappings;
 using KandaIdea_Task.Application.Services;
+using KandaIdea_Task.Application.Validators;
 using KandaIdea_Task.Domain.Interfaces;
 using KandaIdea_Task.Extensions;
 using KandaIdea_Task.Infrastructure.Data;
 using KandaIdea_Task.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using FluentValidation;
+using KandaIdea_Task.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ICityRepository, CityRepository>();
 //DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("kanda")));
 //AutoMapper
 builder.Services.AddAutoMapper(typeof(MappingProfile));
 builder.Services.AddControllers();
+// Fluent Validator
+builder.Services.AddValidatorsFromAssemblyContaining<UserDtoValidator>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -32,7 +38,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseMiddleware<RequestLoggingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
