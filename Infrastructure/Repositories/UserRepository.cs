26a68d2c233d56ef9c5e45a23a62998a8fd71b59
javadfaraahi@gmail.com <javadfaraahi@gmail.com>
2025-04-17 09:26:51 +0330

    Fix UserRepository Problem

--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -1,41 +1,48 @@
 ﻿using KandaIdea_Task.Domain.Entities;
+using KandaIdea_Task.Infrastructure.Data;
 using System.Linq.Expressions;
 
-namespace KandaIdea_Task.Infrastructure.Repositories
+namespace KandaIdea_Task.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
 {
+
     #region Fields
-    private readonly 
+    private readonly AppDbContext _context;
     #endregion
-    public class UserRepository : IUserRepository
+    #region Ctor
+    public UserRepository(AppDbContext context)
     {
-        public Task AddAsync(User user)
-        {
-            throw new NotImplementedException();
-        }
+        _context = context; 
+    }
+    #endregion
+    public Task AddAsync(User user)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+    public Task DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<List<User>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
+    public Task<List<User>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<User> GetAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<User> GetAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<List<User>> GetPatientsByConditionAsync(Expression<Func<User, bool>> predicate)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<List<User>> GetPatientsByConditionAsync(Expression<Func<User, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task UpdateAsync(User user)
-        {
-            throw new NotImplementedException();
-        }
+    public Task UpdateAsync(User user)
+    {
+        throw new NotImplementedException();
     }
 }
