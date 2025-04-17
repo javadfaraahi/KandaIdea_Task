b4ddc975165ee8e7dc76f8f043a3df1de12351a3
javadfaraahi@gmail.com <javadfaraahi@gmail.com>
2025-04-17 12:42:29 +0330

    Make Repositories generic  2

---
+++ Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,63 @@
+﻿using KandaIdea_Task.Domain.Interfaces;
+using KandaIdea_Task.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace KandaIdea_Task.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        #region Fields
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        #endregion
+        #region Ctor
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>(); 
+        }
+        #endregion
+        public virtual async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public virtual IQueryable<T> AsQueryable()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        public virtual async Task DeleteAsync(int id)
+        {
+            var entity = await GetAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public virtual async Task<List<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task<T> GetAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public virtual async Task<List<T>> GetByConditionAsync(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet.Where(predicate).ToListAsync(); 
+        }
+
+        public virtual async Task UpdateAsync(T entity)
+        {
+             _dbSet.Update(entity);
+        }
+        public virtual async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -6,52 +6,22 @@ using System.Linq.Expressions;
 
 namespace KandaIdea_Task.Infrastructure.Repositories;
 
-public class UserRepository : IUserRepository
+public class UserRepository : Repository<User> , IUserRepository
 {
 
-    #region Fields
-    private readonly AppDbContext _context;
-    #endregion
-    #region Ctor
-    public UserRepository(AppDbContext context)
+
+    public UserRepository(AppDbContext context) : base(context)
     {
-        _context = context; 
-    }
-    #endregion
-    public async Task AddAsync(User user)
-    {
-        await _context.Users.AddAsync(user);
-        await _context.SaveChangesAsync();
     }
 
-    public async Task DeleteAsync(int id)
+    public override async Task DeleteAsync(int id)
     {
         var userToDelete = await GetAsync(id);
-        if (userToDelete is not null) 
+        if (userToDelete is not null)
         {
             userToDelete.isDeleted = true;
-            await _context.SaveChangesAsync();
+            await SaveChangesAsync();
         }
     }
-
-    public async Task<List<User>> GetAllAsync()
-    {
-        return await _context.Users.ToListAsync();
-    }
-
-    public async Task<User> GetAsync(int id)
-    {
-       return await _context.Users.FindAsync(id);
-    }
-
-    public async Task<List<User>> GetUsersByConditionAsync(Expression<Func<User, bool>> predicate)
-    {
-        return await _context.Users.Where(predicate).ToListAsync();
-    }
-
-    public async Task UpdateAsync(User user)
-    {
-        _context.Users.Update(user);
-        await _context.SaveChangesAsync();
-    }
+   
 }
