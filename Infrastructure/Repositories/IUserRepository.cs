9c4742e1651685d8dd226ef4132c21ba07648a7e
javadfaraahi@gmail.com <javadfaraahi@gmail.com>
2025-04-17 09:20:34 +0330

    Create UserRepository and IUserRepository

---
+++ Infrastructure/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using KandaIdea_Task.Domain.Entities;
+using System.Linq.Expressions;
+namespace KandaIdea_Task.Infrastructure.Repositories;
+
+public interface IUserRepository
+{
+    Task<List<User>> GetAllAsync();
+    Task<User> GetAsync(int id);
+    Task<List<User>> GetPatientsByConditionAsync(Expression<Func<User, bool>> predicate);
+    Task AddAsync(User user);   
+    Task DeleteAsync(int id);
+    Task UpdateAsync(User user);
+
+}
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using KandaIdea_Task.Domain.Entities;
+using System.Linq.Expressions;
+
+namespace KandaIdea_Task.Infrastructure.Repositories
+{
+    #region Fields
+    private readonly 
+    #endregion
+    public class UserRepository : IUserRepository
+    {
+        public Task AddAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<User>> GetPatientsByConditionAsync(Expression<Func<User, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
