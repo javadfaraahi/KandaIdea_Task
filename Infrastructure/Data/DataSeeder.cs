a4f08e76f7545e2293ba0884de6f22e78d1f8665
javadfaraahi@gmail.com <javadfaraahi@gmail.com>
2025-04-17 10:47:53 +0330

    Add and configure dataseeder

---
+++ Infrastructure/Data/DataSeeder.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DNTPersianUtils.Core.IranCities;
+using KandaIdea_Task.Domain.Entities;
+
+namespace KandaIdea_Task.Infrastructure.Data
+{
+    public static class DataSeeder
+    {
+        public static async Task EnsureSeededAsync(AppDbContext context)
+        {
+            var provinceList = Iran.Provinces.ToList();
+
+            if (!await context.Provinces.AnyAsync()) 
+            {
+                var provincesToAdd = new List<Domain.Entities.Province>();
+                foreach (var province in provinceList)
+                {
+                    provincesToAdd.Add(new Domain.Entities.Province() { Name = province.ProvinceName });
+                }
+                await context.Provinces.AddRangeAsync(provincesToAdd);
+                await context.SaveChangesAsync();
+            }
+            if (!await context.Cities.AnyAsync())
+            {
+                var citiesToAdd = new List<Domain.Entities.City>();
+                foreach (var province in provinceList)
+                {
+                    var existingProvince = await context.Provinces.FirstOrDefaultAsync(x => x.Name == province.ProvinceName);
+                    foreach (var city in province.Counties)
+                    {
+                        citiesToAdd.Add(new Domain.Entities.City() { Name = city.CountyName, Province = existingProvince, ProvinceId = existingProvince.Id });
+                    }
+                }
+                await context.Cities.AddRangeAsync(citiesToAdd);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
