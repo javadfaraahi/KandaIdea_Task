c3b95eddee1bdf2d45072fdeb31d924be0745c1d
Javad Faraahi <javadfaraahi@gmail.com>
2025-04-17 19:30:38 +0330

    Create Province Service

---
+++ Application/Services/ProvinceService.cs
@@ -0,0 +1,25 @@
+﻿using DNTPersianUtils.Core.IranCities;
+using KandaIdea_Task.Application.DTOs;
+using KandaIdea_Task.Domain.Interfaces;
+
+namespace KandaIdea_Task.Application.Services
+{
+    public class ProvinceService : IProvinceService
+    {
+        public Task<List<ProvinceDTO>> ShowProvinceWithCities()
+        {
+            var AllProvince = Iran.Provinces.ToList();
+            var result = new List<ProvinceDTO>();
+            foreach (var province in AllProvince)
+            {
+                var newProvinceDto = new ProvinceDTO() 
+                { 
+                    ProvinceName = province.ProvinceName ,
+                    Cities = province.Counties.Select(x=>x.CountyName).ToList()
+                };
+                result.Add(newProvinceDto);
+            }
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ Domain/Interfaces/IProvinceService.cs
@@ -0,0 +1,9 @@
+﻿using KandaIdea_Task.Application.DTOs;
+
+namespace KandaIdea_Task.Domain.Interfaces
+{
+    public interface IProvinceService
+    {
+        Task<List<ProvinceDTO>> ShowProvinceWithCities();
+    }
+}
